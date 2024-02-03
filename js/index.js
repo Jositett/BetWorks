4e9cb80cb11015e372091565d3367edc1bc972ae
Joedroid <jositett@gmail.com>
2024-02-03 07:36:56 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -486,7 +486,7 @@ function loadTable(csvFile, tableId) {
                     scrollCollapse: true,
                     // autoWidth: false,
                     columnDefs: [{
-                            targets: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, -1, -2, -3, -4, -5, -6, -7, -8, -9],
+                            targets: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14],
                             className: 'shrink-style'
                         },
                         {
